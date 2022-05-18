6e01d6402c622902e64e7e3eebd4ba66468a68ee
Ryu-dai <ryudaimisawa21@gmail.com>
2022-05-18 11:51:00 +0900

    Update dorobou.py

--- dorobou.py
+++ dorobou.py
@@ -1,4 +1,6 @@
-N, W = map(int, input().split()) # N個の品物、Wが重さの容量
+W = int(input())
+N = int(input())
+
 p = [(0, 0)] + [tuple(map(int, input().split())) for _ in range(N)] # 品物-> (価値, 重さ)
 dp = [[0] * (W+1) for _ in range(N+1)] # 縦が品物の番号、横が現在の容量、dpの中身が最大の価値とする
 
